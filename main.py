125189a70ebc9cd2c4e17ec7f14d9ab6118189ab
Luis Felipe Farias Rodrigues <luis.rodrigues@opiceblum.com.br>
2025-10-16 14:01:20 -0300

    adicionando CORSMiddleware

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ import pandas as pd
 import re
 from io import BytesIO
 
+
+from fastapi.middleware.cors import CORSMiddleware
+# ...
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",  # ✅ Seu servidor local
+    "http://localhost:5500",  # ✅ Variação de localhost
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,              
+    allow_credentials=True,             
+    allow_methods=["*"],                
+    allow_headers=["*"],                
+)
 app = FastAPI()
 
 def extract_value(text):
